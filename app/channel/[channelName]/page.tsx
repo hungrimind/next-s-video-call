1b6b4db9d352c199095922201c24a8c9244cbcf3
Tadas Petra <tadas@tadaspetra.com>
2024-04-26 12:35:41 -0500

    Remove bang operator

--- app/channel/[channelName]/page.tsx
+++ app/channel/[channelName]/page.tsx
@@ -6,7 +6,7 @@ export default function Page({ params }: { params: { channelName: string } }) {
   return (
     <main className="flex w-full flex-col">
       <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-neutral-900 dark:text-neutral-100">
-        {params.channelName!}
+        {params.channelName}
       </p>
       <Call channelName={params.channelName}></Call>
     </main>
